8449bb3c300dba7a8075292d360318b6c0f56a5b
Rikozi <48657323+Rikozi@users.noreply.github.com>
2020-04-08 15:39:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
-#
-# quick installer for SET
-#
-#
 import subprocess
 import os
 print("[*] Installing requirements.txt...")
 subprocess.Popen("pip3 install -r requirements.txt", shell=True).wait()
+os.system("clear")
 print("[*] Finished.")
