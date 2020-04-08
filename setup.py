87c89869bd3bc2af60c2beb6a5a4a0f65be28ab2
Rikozi <48657323+Rikozi@users.noreply.github.com>
2020-04-08 16:31:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ import subprocess
 import os
 print("[*] Installing requirements.txt...")
 subprocess.Popen("pip3 install -r requirements.txt", shell=True).wait()
+os.system("sudo apt install netcat")
 os.system("clear")
 print("[*] Finished.")
